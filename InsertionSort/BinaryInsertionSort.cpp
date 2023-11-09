9257cb3a2fcd6ed1b4ee214ea5d7abc5facb2e93
Serhii Nesteruk <yarench228@gmail.com>
2023-11-09 13:50:33 +0100

    Add BinaryInsertionSort

---
+++ InsertionSort/BinaryInsertionSort.cpp
@@ -0,0 +1,36 @@
+#include "../Utils/Utils.inl"
+
+using namespace std;
+
+template <Utils::isNumber T>
+void sort_(T(&arr)[sizeArr])
+{
+	int x = 0, i = 0;
+	for (int j = sizeArr - 2; j >= 0; --j)
+	{
+		x = arr[j];
+		i = j + 1;
+		while ((i < sizeArr) && (x > arr[i]))
+		{
+			arr[i - 1] = arr[i];
+			++i;
+		}
+		arr[i - 1] = x;
+	}
+}
+
+int main() {
+	int arr[sizeArr]{};
+
+	Utils::randArrayInit(arr, 100);
+
+	cout << "Before: ";
+	Utils::print(arr);
+
+	sort_(arr);
+
+	cout << "After: ";
+	Utils::print(arr);
+
+	return 0;
+}
